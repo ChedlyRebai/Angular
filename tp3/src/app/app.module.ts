53f978715901b5a22d70dc3ca9881cae77cb05a4
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2022-10-17 20:38:23 +0200

    add src

--- tp3/src/app/app.module.ts
+++ tp3/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InfoComponent } from './info/info.component';
+import { CommentComponent } from './comment/comment.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    CommentComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
