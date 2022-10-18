f638808d54a02b9ae13998e461d407cd88e75492
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2022-10-18 11:48:01 +0200

    ..

--- magasin/src/app/articles/articles.component.ts
+++ magasin/src/app/articles/articles.component.ts
@@ -8,16 +8,13 @@ import { Article } from '../model/article.model';
 export class ArticlesComponent implements OnInit {
 
   articles : Article[];
- constructor() {
- this.articles = [
- {codea : 1, libelle : "Souris Wifi", prix : 39.100, qte : 8,
-dateAjout : new Date("09/27/2022")},
- {codea : 2, libelle : "Clavier Gaming", prix : 45.900, qte :
-11, dateAjout : new Date("09/30/2022")},
- {codea : 3, libelle : "Manette de jeu", prix : 26, qte : 5,
-dateAjout : new Date("10/02/2022")}
- ];
- }
+  constructor() {
+    this.articles = [
+      {codea : 1, libelle : "Souris Wifi", prix : 39.100, qte : 8,dateAjout : new Date("09/27/2022")},
+      {codea : 2, libelle : "Clavier Gaming", prix : 45.900, qte :11, dateAjout : new Date("09/30/2022")},
+      {codea : 3, libelle : "Manette de jeu", prix : 26, qte : 5,dateAjout : new Date("10/02/2022")}
+    ];
+  }
 
   ngOnInit(): void {
   }
