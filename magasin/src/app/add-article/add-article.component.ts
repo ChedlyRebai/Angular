46245c88cc1e6e9023525c97bf43842a2590e1de
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2022-11-01 08:52:20 +0100

    add tp6

--- magasin/src/app/add-article/add-article.component.ts
+++ magasin/src/app/add-article/add-article.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Article } from '../model/article.model';
+import { ArticleService } from '../services/article.service';
 @Component({
-  selector: 'app-add-article',
-  templateUrl: './add-article.component.html',
-  styleUrls: ['./add-article.component.css']
+ selector: 'app-add-article',
+ templateUrl: './add-article.component.html',
+ styleUrls: ['./add-article.component.css']
 })
 export class AddArticleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ newArticle = new Article();
+ constructor(private articleService: ArticleService ) { }
+ addArticle(){
+ this.articleService.ajouterArticle(this.newArticle);
+ }
+ ngOnInit(): void {
+ }
 }
